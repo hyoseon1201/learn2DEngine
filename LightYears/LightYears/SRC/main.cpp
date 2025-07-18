a81145f363a23f89d4bf4c71c10fca879c9e2d9d
Hyoseon <wink2090@naver.com>
2025-07-18 19:21:09 +0900

    feat: add git ignore and sfml

--- LightYears/LightYears/SRC/main.cpp
+++ LightYears/LightYears/SRC/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::RenderWindow renderWindow{ sf::VideoMode({1280, 720}), "Light Years" };
 	std::cout << "hello world" << std::endl;
+
+	while (renderWindow.isOpen())
+	{
+		while (const auto event = renderWindow.pollEvent())
+		{
+			if (const auto* closedEvent = event->getIf<sf::Event::Closed>())
+			{
+				renderWindow.close();
+			}
+
+			if (const auto* keyPressedEvent = event->getIf<sf::Event::KeyPressed>())
+			{
+				if (keyPressedEvent->code == sf::Keyboard::Key::Escape)
+				{
+					std::cout << "ESC key pressed!" << std::endl;
+					renderWindow.close();
+				}
+			}
+		}
+
+		renderWindow.clear(sf::Color::Blue);
+		renderWindow.display();
+	}
+
+	return 0;
 }
--- LightYears/build/cmake_install.cmake
+++ LightYears/build/cmake_install.cmake
@@ -34,6 +34,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("E:/learn2DEngine/LightYears/build/_deps/sfml-build/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("E:/learn2DEngine/LightYears/build/LightYears/cmake_install.cmake")
 endif()
 
